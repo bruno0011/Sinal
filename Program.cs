5c73f947549ced8cac32d6f564efb17fde552ee3
bruno0011 <brunosouzviana@hotmail.com>
2022-04-01 22:15:05 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿int numeroDigitado;
+
+Console.Write("Numero digitado:");
+numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+if (numeroDigitado < 0)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Negativo!");
+
+}
+else if (numeroDigitado > 0)
+{
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.WriteLine("Positivo!");
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.DarkMagenta;
+    Console.WriteLine("Zero!");
+}
+Console.ResetColor();
